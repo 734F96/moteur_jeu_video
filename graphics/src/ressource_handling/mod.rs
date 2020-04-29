2e98a650382b7f70c7c20a3333904285ea5abd88
technolapin <clement.chomicki@protonmail.com>
2020-04-29 11:20:21 +0000

    Mon pc a fait sepuku, je sauve mon travail en cours en chroot

--- graphics/src/ressource_handling/mod.rs
+++ graphics/src/ressource_handling/mod.rs
@@ -7,6 +7,7 @@ pub mod scene;
 pub mod wavefront;
 pub mod tile;
 pub mod holder;
+pub mod light;
 
 pub use vertex::*;
 pub use material::*;
@@ -17,3 +18,4 @@ pub use ressources_holder::*;
 pub use scene::*;
 pub use tile::*;
 pub use holder::*;
+pub use light::*;
--- graphics/src/ressource_handling/scene.rs
+++ graphics/src/ressource_handling/scene.rs
@@ -1,22 +1,26 @@
-use super::Object;
-use crate::engine::{Graphical, Frame, Camera};
+use super::{Object, Light, Lights, N_MAX_LIGHTS};
+use crate::engine::{Graphical, Frame, Camera, Display};
 use crate::misc::{Similarity, new_vertexbuffer};
 
+use glium::uniforms::UniformBuffer;
 
 /**
 A scene contains pointers to existing ressources and datas to place them in the space.
 */
 pub struct Scene {
     pub objects: Vec<(Vec<Object>, Vec<Similarity>)>,
+    pub lights: Lights,
     pub camera: Camera
 }
 
 
 impl Scene {
     /// creates a scene
-    pub fn new() -> Self {
+    pub fn new(disp: &Display) -> Self
+    {
         Self {
             objects: Vec::new(),
+            lights: Lights::new(&disp.display),
             camera: Camera::new(2.0)
         }
     }
@@ -26,6 +30,11 @@ impl Scene {
         self.objects.push((meshes, instances));
     }
 
+    pub fn add_light(&mut self, light: Light)
+    {
+        self.lights.push(light);
+    }
+    
     pub fn update_aspect_ratio(&mut self, gr: &Graphical)
     {
         self.camera.update_aspect_ratio(gr);
@@ -34,16 +43,19 @@ impl Scene {
     /// Makes the graphic engine renders the scene. (maybe a bad idea)
     pub fn render(&mut self, gr: &Graphical, frame: &mut Frame)
     {
+
+        //self.lights.print();
+
+        
         self.camera.update_aspect_ratio(gr);
         self.objects.iter().for_each(|(objects, instances)| {
             let vbo = new_vertexbuffer(&gr.display, instances);
             objects
                 .iter()
                 .for_each(|ob|
-                          frame.draw(&gr, &ob, &vbo, &self.camera)
+                          frame.draw(&gr, &ob, &vbo, &self.camera, &self.lights)
                 )
         });
-        
     }
     
 }
